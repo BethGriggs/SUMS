ba00460b7eedc4f44388023130d9f13e7026e281
Bethany Nicolle Griggs <bethanygriggs@outlook.com>
2016-03-16 12:01:26 +0000

    add ajax to deselect idea

--- build/generated-sources/ap-source-output/up678526/sums/ents/Idea_.java
+++ build/generated-sources/ap-source-output/up678526/sums/ents/Idea_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import up678526.sums.ents.Person;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-16T00:43:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-16T12:00:54")
 @StaticMetamodel(Idea.class)
 public class Idea_ { 
 
--- build/generated-sources/ap-source-output/up678526/sums/ents/Person_.java
+++ build/generated-sources/ap-source-output/up678526/sums/ents/Person_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import up678526.sums.ents.Idea;
 import up678526.sums.ents.Organisation;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-16T00:43:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-16T12:00:54")
 @StaticMetamodel(Person.class)
 public class Person_ { 
 
