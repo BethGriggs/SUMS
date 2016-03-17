59a8464bba986cffedfbea702c17711803ef9c36
Bethany Nicolle Griggs <bethanygriggs@outlook.com>
2016-03-17 19:47:57 +0000

    change title

--- build/generated-sources/ap-source-output/up678526/sums/ents/Organisation_.java
+++ build/generated-sources/ap-source-output/up678526/sums/ents/Organisation_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-16T21:55:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-17T19:25:39")
 @StaticMetamodel(Organisation.class)
 public class Organisation_ { 
 
